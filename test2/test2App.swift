a17e7cb8c55b52333e3fceeb50edb612f8a9e4fd
lcj <lcj@kth.se>
2022-12-15 12:34:50 +0100

    Initial Commit

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Leandro Carocca Jeria on 2022-12-15.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
